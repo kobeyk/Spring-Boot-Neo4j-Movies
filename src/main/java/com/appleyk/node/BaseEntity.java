320bbc639195d910407e30d9bc09c15e8e242c9c
Xiang Li <lx3103@gmail.com>
2019-02-25 04:18:46 +0800

    替换deprecated接口

--- src/main/java/com/appleyk/node/BaseEntity.java
+++ src/main/java/com/appleyk/node/BaseEntity.java
@@ -1,7 +1,6 @@
 package com.appleyk.node;
 
-import org.neo4j.ogm.annotation.GraphId;
-
+import org.neo4j.ogm.annotation.Id;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
@@ -14,8 +13,7 @@ public abstract class BaseEntity {
 	/**
 	 * Neo4j会分配的ID（节点唯一标识 当前类中有效）
 	 */
-	@GraphId
-	
+	@Id
 	private Long id;	
 	public Long getId() {
 		return id;
